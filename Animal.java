fe712b1a807d30dc72364dc8c904ebe9bf533d93
lourdessaa11 <saa21333@uvg.edu.gt>
2021-09-28 09:58:15 -0600

    Creación de Clase Animal

--- Animal.java
+++ Animal.java
@@ -8,5 +8,55 @@ Clase animal
 ******************************************************************/
 
 public class Animal {
-    
+ 
+    private String nombre;
+    private String descripcion;
+    private String informacion;
+
+
+    public Animal(){
+
+        nombre = "";
+        descripcion = "";
+        informacion = "";
+
+    }
+
+    //Getters y Setters
+    public void setNombre(String nom){
+
+        nombre = nom;
+
+    }
+
+    public String getNombre(){
+
+        return nombre;
+
+    }
+
+    public void setDescripcion(String des){
+
+        nombre = des;
+
+    }
+
+    public String getDescripcion(){
+
+        return descripcion;
+
+    }
+
+    public void setInformacion(String info){
+
+        nombre = info;
+
+    }
+
+    public String getInformacion(){
+
+        return informacion;
+
+    }
+
 }
