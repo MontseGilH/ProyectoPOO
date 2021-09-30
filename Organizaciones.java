8b61f3b6aa2624f389d9fcca510ca2dbaa052c38
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2021-09-30 07:00:19 -0600

    Actualizacion de organizaciones

--- Organizaciones.java
+++ Organizaciones.java
@@ -1,4 +1,15 @@
+/******************************************************************
+Ultima modificacion: 28/09/2021
+
+Clase organizaciones
+@file Organizaciones.java
+@author Jose
+@version 1.0, 29/09/2021
+******************************************************************/
+
+
 public class Organizaciones {
+
     private String Nombre;
     private String AnnioDeCreacion;
     private String Lugar;
@@ -6,41 +17,41 @@ public class Organizaciones {
     private String Informacion;
 
     public Organizaciones(String Nombre, String AnnioDeCreacion, String Lugar, String InformacionDeContacto, String Informacion) {
-    this.setNombre(Nombre);
-    this.setAnnioDeCreacion(AnnioDeCreacion);
-    this.setLugar(Lugar);
-    this.setInformacionDeContacto(InformacionDeContacto);
-    this.setInformacion(Informacion);
-}
+        this.setNombre(Nombre);
+        this.setAnnioDeCreacion(AnnioDeCreacion);
+        this.setLugar(Lugar);
+        this.setInformacionDeContacto(InformacionDeContacto);
+        this.setInformacion(Informacion);
+    }
     public void setNombre(String nom){
         this.Nombre=nom;
-}
+    }
     public String getNombre(){
         return Nombre;
-}
+    }
     public void setAnnioDeCreacion(String Annio){
         this.AnnioDeCreacion=Annio;
-}
+    }
     public String getAnnioDeCreacion(){
         return AnnioDeCreacion;
-}
+    }
     public void setLugar(String Lug){
         this.Lugar=Lug;
-}
+    }
     public String getLugar(){
         return Lugar;
-}
+    }
     public void setInformacionDeContacto(String InfoC){
         this.InformacionDeContacto=InfoC;
-}
+    }
     public String getInformacionDeContacto() {
         return InformacionDeContacto;
-}
+    }
     public void setInformacion(String Info){
         this.Informacion=Info;
-}
+    }
     public String getInformacion(){
         return Informacion;
-}
+    }
 
-}
+    }
