84a5dd66a416de087fca9ccec43950ec1eff2fbf
her21105 <87708535+her21105@users.noreply.github.com>
2021-09-30 14:16:27 -0600

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -10,8 +10,9 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.io.File;
 
-public class Manager {
+public class Manager<File><File> {
     
     public void ingresarUsuario(Usuario u){
         String s = u.userString();
@@ -22,4 +23,71 @@ public class Manager {
             System.out.println("Error al ingresar usuario al file");
         }
     }
+
+    //vaiables
+    boolean error;
+
+    //ruta
+    String rutaAnimales = "animales.txt";//nombre de archivo donde se guardan los animales
+    String rutaUsuarios = "usuarios.txt";
+
+    //File
+    File fileanimal = new File(rutaAnimales);
+
+
+    //constructor
+    Manager(){
+
+    }
+
+    //verificar existencia de archivo
+    public boolean archivoExisteAnimales(){
+        
+        error = false;
+
+        try{
+            if(!fileanimal.exists()){
+            
+                fileanimal.createNewFile();
+    
+            }
+        }
+        catch(IOException e){
+
+            error = true;
+
+        }
+        
+
+        return error;
+        
+    }
+
+
+    //ingresa animales
+    public boolean ingresarAnimal(String nombre, String descripcion, String informacion){
+
+        error = false;
+
+        try{
+
+            FileWriter fw = new FileWriter(fileanimal);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(nombre);
+            bw.write(descripcion);
+            bw.write(informacion);
+            bw.close();
+
+        }catch(IOException e){
+
+            error = true;
+
+        }
+
+        return error;
+    }
+
+
+}
+
 }
