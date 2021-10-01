c093c1f76ad5d5759f9d3ffca108b717db81e7f3
JoseJMel <87709501+JoseJMel@users.noreply.github.com>
2021-10-01 12:12:24 -0600

    Comentarios en Organizaciones
    Descripción de los métodos en la clase Organizaciones y otro cambios

--- Organizaciones.java
+++ Organizaciones.java
@@ -23,37 +23,71 @@ public class Organizaciones {
         this.setInformacionDeContacto(informacionDeContacto);
         this.setInformacion(informacion);
     }
+    /* Asigna el valor a la variable nombre
+     * @Param nom the nombre to set
+     */
+
     public void setNombre(String nom){
         this.nombre=nom;
     }
+    /* Devuelve el valor de la variable nombre
+    * @return the nombre
+    */
     public String getNombre(){
         return nombre;
     }
+    /* Asigna valor a la variable AnnioDeCreacion
+    * @Param annio the annioDeCreacion to set
+     */
     public void setAnnioDeCreacion(String annio){
         this.annioDeCreacion=annio;
     }
+    /* Devuelve el valor de la variable annioDeCreacion
+    * @return the annioDeCreacion
+     */
     public String getAnnioDeCreacion(){
         return annioDeCreacion;
     }
+    /* Asigna el valor a la variable lugar
+    * @Param lug the lugar to set
+     */
     public void setLugar(String lug){
         this.lugar=lug;
     }
+    /* Devuelve el valor de la variable lugar
+    *@return the lugar
+     */
     public String getLugar(){
         return lugar;
     }
+    /* Asigna el valor a la variable informacionDeContacto
+    *@Param infoC the informacionDeContacto to set
+     */
     public void setInformacionDeContacto(String infoC){
         this.informacionDeContacto=infoC;
     }
+    /* Devuelve el valor de la variable informacionDeContacto
+    *@return othe informacionDeContacto
+     */
     public String getInformacionDeContacto() {
         return informacionDeContacto;
     }
+    /* Asigna el valor de la variable informacion
+    *@Param info the informacion to set
+     */
     public void setInformacion(String info){
         this.informacion=info;
     }
+    /* Devuelve el valor de la variable informacion
+    *@return the informacion
+     */
     public String getInformacion(){
         return informacion;
     }
 
+    /* Asigna valor a la variable s
+    @return the s
+     */
     public String organizacionesString(){
         String s = "\n"+this.getNombre()+"|"+this.getAnnioDeCreacion()+"|"+this.getLugar()+"|"+this.getInformacionDeContacto()+"|"+this.getInformacion();
         return s;
